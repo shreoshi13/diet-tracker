67f0d4c99066abadd7fcba6c5aaf2c129c4bba02
shreoshi roy <shreoshi63@gmail.com>
2023-09-01 11:02:16 +0530

    ..

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -3,25 +3,77 @@
     import { FiArrowRight } from "react-icons/fi";
     
     const Home = () => {
+
       return (
-        <div className="home-container">
-          <Navbar />
-          <div className="home-banner-container">
-            
-            <div className="home-text-section">
-              <h1 className="primary-heading">
-                Happy Life Starts with Health 
-              </h1>
-              <p className="primary-text">
-                Healthy food is essential for the fitness of your mind, body and heart.
-              </p>
-              <button className="secondary-button">
-                Start Following Now <FiArrowRight />{" "}
-              </button>
+       <div>
+          <div className="container">
+            <div className="row">
+              <div className="col-md-6 my-auto">
+                <h1 className="display-4 fw-bold">Good health starts with what you eat.</h1>
+                <button className="btn btn-primary btn-lg">Get Started</button>
+              </div>
+              <div className="col-md-6">
+                <img className="img-fluid" src="https://www.myfitnesspal.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fhero-slice%402x.e3eec8a8.png&w=1920&q=75" alt="" />
+              </div>
             </div>
-            
           </div>
-        </div>
+          <div className="container">
+            <div className="row">
+              
+              <div className="col-md-6">
+                <img className="img-fluid" src="https://www.myfitnesspal.com/_next/image?url=%2Fpages%2Fhome%2Fsecondary-hero-slice%402x.png&w=512&q=75" alt="" />
+              </div>
+              <div className="col-md-6 my-auto">
+                <h1 className="display-4 fw-bold">Happy Life Starts with Health .</h1>
+                <button className="btn btn-primary btn-lg">Start Following Now</button>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+  <div className="col-sm-3 mb-3 mb-sm-0">
+    <div className="card text-center">
+    <img className="img-fluid rounded-circle" src="https://www.myfitnesspal.com/_next/static/media/diary.e6e4c438.svg" alt="" />
+
+      <div className="card-body">
+        <h5 className="card-title">Learn. Track. Improve.</h5>
+        <p className="card-text">
+        Keeping a food diary helps you understand your habits and increases your likelihood of hitting your goals.
+        </p>
+        
+      </div>
+    </div>
+  </div>
+  <div className="col-sm-3">
+    <div className="card text-center">
+    <img className="img-fluid" src="https://www.myfitnesspal.com/_next/static/media/barcode.18277ae4.svg"/>
+      <div className="card-body">
+        <h5 className="card-title">Logging Simplified</h5>
+        <p className="card-text">
+        Scan barcodes, save meals and recipes, and use Quick Tools for fast and easy food tracking.
+        </p>
+       
+      </div>
+    </div>
+  </div>
+  <div className="col-sm-3">
+    <div className="card text-center">
+      <div>
+    <img className="rounded-circle shadow" style={{objectFit: 'contain'}} src="https://www.myfitnesspal.com/_next/static/media/shoeblue.28e2684f.svg"/>
+    </div>
+      <div className="card-body">
+        <h5 className="card-title">Stay Motivated</h5>
+        <p className="card-text">
+        Join the World’s Largest Fitness Community for advice, tips, and support 24/7.
+        </p>
+       
+      </div>
+    </div>
+  </div>
+</div>
+
+        
+        
+       </div>
       );
     };
     
