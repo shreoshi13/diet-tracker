a8c601f9dafa95d6eeac4aec7b1e78be9e6feebb
shreoshi roy <shreoshi63@gmail.com>
2023-09-17 10:06:40 +0530

    changes

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -205,12 +205,6 @@ const Home = () => {
             />
           </SwiperSlide>
 
-          {/* <div className="autoplay-progress" slot="container-end">
-          <svg viewBox="0 0 48 48" ref={progressCircle}>
-            <circle cx="24" cy="24" r="20"></circle>
-          </svg>
-          <span ref={progressContent}></span>
-        </div> */}
         </Swiper>
       </div>
 
